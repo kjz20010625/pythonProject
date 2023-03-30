a83d41779d7ce49e3a5f65f167c268ce612fefbf
jungu <kjz20010625@outlook.com>
2023-03-30 16:24:05 +0800

    Initial commit

---
+++ operation.py
@@ -0,0 +1,34 @@
+# age = 17
+# if age >= 18:
+#     print("已经成年可以上网！")
+# else:
+#     print("未成年，不能上网,悄悄上网")
+#
+# x = 0
+# while x < 10:
+#     x = x + 1
+#     print(x)
+#
+# for x in (0, 1, 2, 3, 4):
+#     print(x)
+#
+# for x in range(1, 5):
+#     print(x)
+#
+# for i in range(5):
+#     print("......")
+#     print(i)
+# i = 1
+for i in range(5):
+    i += 1
+    print("......")
+    ifi == 3
+    break
+    print(i)
+
+count = 0
+while count < 5:
+    print(count, "is less than5")
+    count = count + 1
+else:
+    print(count, "is not less than5")
